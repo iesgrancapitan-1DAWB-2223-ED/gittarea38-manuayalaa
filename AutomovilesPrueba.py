bb69e59a165e28de5fcc552ffc7ce2399f790ae4
manuayalaa <115107799+manuayalaa@users.noreply.github.com>
2023-03-02 19:10:02 +0100

    Add files via upload

---
+++ AutomovilesPrueba.py
@@ -0,0 +1,22 @@
+class automoviles():
+    def __init__(self, motor, marca, modelo):
+        self.motor = motor
+        self.marca = marca
+        self.modelo = modelo
+        self.arrancado = False
+
+    def arrancar(self):
+        if self.arrancado == True:
+            print("Estoy arrancado")
+        else:
+            print("Estoy arrancando")
+            self.arrancando = True
+
+    def parar(self):
+        if self.arrancado == False:
+            print("Estoy parado")
+        else:
+            print("Estoy parando")
+
+
+
